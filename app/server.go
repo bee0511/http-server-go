dd1e7f7bbed4728b66cf05e4dc23dce454eb9864
bee0511 <bee000092.cs10@nycu.edu.tw>
2024-07-03 12:09:26 +0800

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -1,21 +1,36 @@
 package main
 
 import (
-	"fmt"
-	"net"
-	"os"
+    "fmt"
+    "net"
+    "os"
 )
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
-	}
-	
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+    l, err := net.Listen("tcp", "0.0.0.0:4221")
+    if err != nil {
+        fmt.Println("Failed to bind to port 4221:", err)
+        os.Exit(1)
+    }
+    defer l.Close()
+    fmt.Println("Listening on port 4221...")
+
+    for {
+        conn, err := l.Accept()
+        if err != nil {
+            fmt.Println("Error accepting connection:", err)
+            continue
+        }
+
+        go handleConnection(conn)
+    }
+}
+
+func handleConnection(conn net.Conn) {
+    defer conn.Close()
+    response := "HTTP/1.1 200 OK\r\n"
+    _, err := conn.Write([]byte(response))
+    if err != nil {
+        fmt.Println("Error writing response:", err)
+    }
 }
