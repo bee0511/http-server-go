b6d1131dc2876ad05c7d17aa648b057014c90fd3
bee0511 <bee000092.cs10@nycu.edu.tw>
2024-07-04 12:24:26 +0800

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -23,17 +23,18 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleClient(conn)
 	}
-    go handleClient(conn)
-
 }
 
 func handleClient(conn net.Conn) {
-    defer conn.Close()
+	defer conn.Close()
 	scanner := bufio.NewScanner(conn)
 	req, _ := parseStatus(scanner)
 	// fmt.Println(req.Headers)
